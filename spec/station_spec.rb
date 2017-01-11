5a2452529f1aee78e30d032fe2d8fabf29fb4eeb
konradschlatte <konrad.schlatte@btinternet.com>
2017-01-11 12:02:04 +0000

    station class

---
+++ spec/station_spec.rb
@@ -0,0 +1,9 @@
+require "station"
+
+describe Station do
+subject(:station) { described_class.new("station",1) }
+
+it { is_expected.to respond_to(:name)}
+it { is_expected.to respond_to(:zone)}
+
+end
