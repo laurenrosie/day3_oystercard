4ce68994e0f72eedaf551d65be6f4116528f8a24
Lauren Rosie <laurenrosiewatson@gmail.com>
2017-01-11 14:46:25 +0000

    refactoring

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,22 +3,20 @@ require "./lib/journey"
 class Oystercard
 MAX_BALANCE = 90
 MIN_BALANCE = 1
-attr_reader :balance, :money, :journeys, :current_journey
+attr_reader :balance, :journeys, :current_journey
 
   def initialize
     @balance = 0
-    @money = 0
     @journeys = []
     @current_journey = nil
   end
 
   def top_up(money)
-  @money = money
-  raise "the maximum allowed balance is #{MAX_BALANCE} !" if over_limit?
+  raise "the maximum allowed balance is #{MAX_BALANCE} !" if over_limit?(money)
   @balance += money
   end
 
-  def over_limit?
+  def over_limit?(money)
     balance + money > MAX_BALANCE
   end
 
@@ -27,22 +25,34 @@ attr_reader :balance, :money, :journeys, :current_journey
   end
 
   def touch_in(station)
-    self.touch_out(nil) if current_journey != nil#if current journey NOT nil, then start a new journey with no entry point
+    self.touch_out(nil) if !journey_nil?#if current journey NOT nil, then start a new journey with no entry point
     raise "Unsuffient balance. Top up to at least #{MIN_BALANCE}!" if below_minimum?
     @current_journey = Journey.new(station)
   end
 
   def touch_out(exit_station)
-    @current_journey = Journey.new(nil) if @current_journey == nil
-    @current_journey.finish(exit_station)
+    check_and_end_journey(exit_station)
     deduct(current_journey.fare)
-    @journeys << {entry_station: current_journey.entry_station, exit_station: current_journey.exit_station}
-    @current_journey = nil
+    update_history
   end
+
 
   private
   def deduct(fare)
     @balance -= fare
   end
 
+  def journey_nil?
+    @current_journey == nil
+  end
+
+  def update_history
+    @journeys << {entry_station: current_journey.entry_station, exit_station: current_journey.exit_station}
+    @current_journey = nil
+  end
+
+  def check_and_end_journey(exit_station)
+    @current_journey = Journey.new(nil) if journey_nil?
+    @current_journey.finish(exit_station)
+  end
 end
